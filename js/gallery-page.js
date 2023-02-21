b2be59e02b34e42cf85d79449503dce43158c3f2
P1l3T0 <vgan3v@gmail.com>
2023-02-21 21:38:45 +0200

    napravih nqkoi neshta (dobre se poly4i)

--- js/gallery-page.js
+++ js/gallery-page.js
@@ -1,23 +1,25 @@
-lazyLaoding()
+lazyLaoding();
 
+
+//!murzelivo zarejdane na snimkite
 
 function lazyLaoding() {
-    const targets = [...document.getElementsByTagName('img')]
+    const targets = [...document.getElementsByTagName('img')];
     
     const lazyLoad = target => {
         const io = new IntersectionObserver((entries, observer) => {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
-                    const img = entry.target
-                    const src = img.getAttribute('data-lazy')
+                    const img = entry.target;
+                    const src = img.getAttribute('data-lazy');
                     
-                    img.setAttribute('src', src)
-                    observer.disconnect()
-                }
-            })
-        })
-        io.observe(target)
+                    img.setAttribute('src', src);
+                    observer.disconnect();
+                };
+            });
+        });
+        io.observe(target);
     }
     
-    targets.forEach(lazyLoad)
+    targets.forEach(lazyLoad);
 }
--- js/home-page.js
+++ js/home-page.js
@@ -1,27 +1,32 @@
-document.querySelector('.section-style .button').addEventListener('click', (event) => {
+document.querySelector(".section-style .button").addEventListener("click", (event) => {
     event.preventDefault();
-    alert('Welcome to Pileshkiq! This website is still under construction, but check back soon for more updates!');
+    alert(
+        "Welcome to Pileshkiq! This website is still under construction"
+        + "but check back soon for more updates!"
+    );
 });
 
-// Display a random chicken fact on the homepage
+//!random fakt za piletata
+
 const chickenFacts = [
     "Chickens are the closest living relatives to the T-Rex dinosaur!",
     "Chickens have a natural ability to see over 300 degrees, without moving their heads!",
     "The record for most eggs laid in one day by a single chicken is 7!",
     "Chickens have a strong memory and can recognize over 100 different faces of people or animals!",
-    "Chickens are very social animals and have their own unique ways of communicating with each other!"
+    "Chickens are very social animals and have their own unique ways of communicating with each other!",
+    "A group of chickens is called a flock!",
+    "Chickens have their own unique language, consisting of over 30 different calls and sounds!",
+    "Hens can communicate with their chicks while they are still in the egg!",
+    "Chickens can remember and recognize over 100 different individuals, including humans!",
+    "Chickens are social animals and can form strong bonds with each other!",
+    "Chickens have excellent eyesight and can see in color!",
+    "Chickens can run up to 9 miles per hour!",
+    "Chickens are capable of problem-solving and can even understand cause and effect relationships!"
 ];
 
-const factDisplay = document.querySelector('.fact-display');
+const factDisplay = document.querySelector(".fact-display");
 
 if (factDisplay) {
     const randomFact = chickenFacts[Math.floor(Math.random() * chickenFacts.length)];
     factDisplay.textContent = randomFact;
 }
-
-document.getElementById("showMore").addEventListener('mouseover', (event) => {
-
-}); 
-
-
-

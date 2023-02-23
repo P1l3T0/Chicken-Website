0da5be776a231c8331ce49a7d6da09aba2b98e41
P1l3T0 <vgan3v@gmail.com>
2023-02-23 23:11:13 +0200

    top breeds page

--- js/gallery-page.js
+++ js/gallery-page.js
@@ -5,21 +5,34 @@ lazyLaoding();
 
 function lazyLaoding() {
     const targets = [...document.getElementsByTagName('img')];
-    
+
     const lazyLoad = target => {
         const io = new IntersectionObserver((entries, observer) => {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
                     const img = entry.target;
                     const src = img.getAttribute('data-lazy');
-                    
+
                     img.setAttribute('src', src);
                     observer.disconnect();
                 };
             });
         });
         io.observe(target);
-    }
-    
+    };
     targets.forEach(lazyLoad);
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));

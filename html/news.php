4370413a709612c5fdfd721978c3da7859b0285e
P1l3T0 <vgan3v@gmail.com>
2023-03-11 13:52:21 +0200

    converting font-size, padding and margin to rem

--- html/news.php
+++ html/news.php
@@ -15,34 +15,51 @@
 
     <div class="container">
         <div class="card">
-            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0"><img src="https://picsum.photos/400/300?random=1"></a>
-            <h2>Local chicken farmer wins award</h2>
-            <p>Spechelih pari sq otivam da investiram v crypto (shte izgybq vsichko)</p>
+            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">
+                <img src="https://picsum.photos/400/300?random=1">
+                <h2>Local chicken farmer wins award</h2>
+                <p>spechelih pari sq otivam da investiram v crypto (shte izgybq vsichko)</p>
+            </a>
         </div>
+
         <div class="card">
-            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0"><img src="https://picsum.photos/400/300?random=2"></a>
-            <h2>New chicken breed discovered</h2>
-            <p>prqk naslednik na dinozavrite (shte stane strahotna pleskavica ot nego)</p>
+            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">
+                <img src="https://picsum.photos/400/300?random=2">
+                <h2>New chicken breed discovered</h2>
+                <p>prqk naslednik na dinozavrite (shte stane strahotna pleskavica ot nego)</p>
+            </a>
         </div>
+
         <div class="card">
-            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0"><img src="https://picsum.photos/400/300?random=3"></a>
-            <h2>Chicken industry sees record profits</h2>
-            <p>Imat pari ygajdat si</p>
+            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">
+                <img src="https://picsum.photos/400/300?random=3">
+                <h2>Chicken industry sees record profits</h2>
+                <p>Imat pari ygajdat si</p>
+            </a>
         </div>
+
         <div class="card">
-            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0"><img src="https://picsum.photos/400/300?random=4"></a>
-            <h2>Chicken prices expected to rise</h2>
-            <p>Tupata inflaciq brat :(</p>
+            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">
+                <img src="https://picsum.photos/400/300?random=4">
+                <h2>Chicken prices expected to rise</h2>
+                <p>Tupata inflaciq brat :(</p>
+            </a>
         </div>
+
         <div class="card">
-            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0"><img src="https://picsum.photos/400/300?random=5"></a>
-            <h2>Chicken farms adapt to climate change</h2>
-            <p>kokoa6arnicite na pernatite glypaci stavat vse po moderni</p>
+            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">
+                <img src="https://picsum.photos/400/300?random=5">
+                <h2>Chicken farms adapt to climate change</h2>
+                <p>kokoa6arnicite na pernatite glypaci stavat vse po moderni</p>
+            </a>
         </div>
+
         <div class="card">
-            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0"><img src="https://picsum.photos/400/300?random=6"></a>
-            <h2>Is organic chicken worth the extra cost?</h2>
-            <p>QDEM SAMO ISTINSKO PILESHKO ILI SMURT</p>
+            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">
+                <img src="https://picsum.photos/400/300?random=6">
+                <h2>Is organic chicken worth the extra cost?</h2>
+                <p>QDEM SAMO ISTINSKO PILESHKO ILI SMURT</p>
+            </a>
         </div>
     </div>
 
